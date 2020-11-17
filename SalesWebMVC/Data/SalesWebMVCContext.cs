14de1e71d80453bc3070155ea95b9a679c17f6a9
Davi <davifrancisco214@outlook.com>
2020-11-16 23:39:37 -0300

    Adicionando novas entidades e realizando a segunda migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Data
 {
+    // Representa a conexão com o banco.
     public class SalesWebMVCContext : DbContext
     {
         public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
@@ -14,6 +11,9 @@ namespace SalesWebMVC.Data
         {
         }
 
+        // Representa as tabelas do banco de dados.
         public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,8 +1,35 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name, ICollection<Seller> sellers)
+        {
+            Id = id;
+            Name = name;
+            Sellers = sellers;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            // Retorna o total das vendas de um determinado departamento
+            return Sellers.Sum(s => s.TotalSales(initial, final));
+        } 
     }
 }
