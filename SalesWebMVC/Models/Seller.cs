311372908e969e0496fc62a2148f4925afdbe1f3
Davi <davifrancisco214@outlook.com>
2020-11-18 00:06:39 -0300

    Adicionando alguns DataAnnotations

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,9 +8,15 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
+        [Display(Name = "Data de Nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Salário Base")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         // Representa qualquer tipo de coleção
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
